2e896ba0db1d5c537a3943a923d9f8113346ef7b
CT-Dev1 <ryanhrousseau@gmail.com>
2023-11-21 21:59:20 +0000

    second edits

--- players/ST449_Player_XXXXX.py
+++ players/ST449_Player_XXXXX.py
@@ -5,4 +5,15 @@ Several Required Functions:
 
 - Take Inspiration from Aggressive Bot
 
+Function 1: Generate Move
+- assumes player hands are A and B
+
 '''
+
+
+def generate_move(state_str):
+    state = {'A': int(state_str[1]), 'B': int(state_str[3]), 'C': int(state_str[5]), 'D': int(state_str[7])}
+    possible_moves = ['AB', 'AC', 'AD', 'BA', 'BC', 'BD']
+
+
+
